409e5599c0218ebe2d4cbfebf1c25e5f6a61ff26
derlistigelurch <48289242+derlistigelurch@users.noreply.github.com>
2019-11-08 11:06:51 +0100

    Add files via upload

---
+++ drop_sequence_script.sql
@@ -0,0 +1,14 @@
+-- drop sequences
+DROP SEQUENCE hersteller_seq;
+DROP SEQUENCE schaden_seq;
+DROP SEQUENCE preisliste_seq;
+DROP SEQUENCE status_seq;
+DROP SEQUENCE adresse_seq;
+DROP SEQUENCE standort_seq;
+DROP SEQUENCE person_seq;
+DROP SEQUENCE kunden_seq;
+DROP SEQUENCE mitarbeiter_seq;
+DROP SEQUENCE exemplar_seq;
+DROP SEQUENCE verleih_seq;
+DROP SEQUENCE exemp_schaden_seq;
+DROP SEQUENCE auto_details_seq;
