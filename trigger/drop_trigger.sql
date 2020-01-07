71751df3abd8f9690bd9a2e54000454b37417716
derlistige lurch <dalistige@gmail.com>
2020-01-07 08:24:20 +0100

    updated drop trtigger script

--- trigger/drop_trigger.sql
+++ trigger/drop_trigger.sql
@@ -1,5 +1,5 @@
-DROP TRIGGER tr_as_u_person;
-DROP TRIGGER tr_as_u_adresse;
+DROP TRIGGER tr_as_u_person_adresse;
+DROP TRIGGER tr_as_u_person_name;
 DROP TRIGGER tr_as_i_auto_verleihen;
 DROP TRIGGER tr_as_i_kunde_erstellen;
 
