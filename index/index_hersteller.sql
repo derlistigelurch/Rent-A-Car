20e4db4cfdff2388595edaa036bc2e6106933cef
derlistigelurch <dalistige@gmail.com>
2020-01-07 07:34:09 +0100

    fixed typos, added fk constraint, added index

---
+++ index/index_hersteller.sql
@@ -0,0 +1,9 @@
+/*********************************************************************
+/**
+/** Name: hersteller_bezeichnung_index
+/** Table: HERSTELLER
+/** Attributes: BEZEICHNUNG
+/**
+/*********************************************************************/
+CREATE INDEX hersteller_bezeichnung_index
+ON HERSTELLER(BEZEICHNUNG);
--- packages/pa_err.sql
+++ packages/pa_err.sql
@@ -10,25 +10,25 @@ AS
   /*********************************************************************
   /**
   /** Procedure: sp_err_handling
-  /** In: i_n_err_code - Fehlercode
-  /** In: i_v_err_msg - Fehlermeldung
+  /** In: l_n_err_code - Fehlercode
+  /** In: l_v_err_msg - Fehlermeldung
   /** Developer: 
   /** Description: Speichert die Fehlermeldung + Fehlercode in eine Tabelle
   /**
   /**********************************************************************/
-  PROCEDURE sp_err_handling (i_n_err_code IN NUMBER, i_v_err_msg IN VARCHAR2);
+  PROCEDURE sp_err_handling (l_n_err_code IN NUMBER, l_v_err_msg IN VARCHAR2);
   END pa_err;
 /
 
 CREATE OR REPLACE PACKAGE BODY pa_err
 AS
   /* sp_err_handling definition *******************************************/
-  PROCEDURE sp_err_handling (i_n_err_code IN NUMBER, i_v_err_msg IN VARCHAR2)
+  PROCEDURE sp_err_handling (l_n_err_code IN NUMBER, l_v_err_msg IN VARCHAR2)
   AS
     PRAGMA AUTONOMOUS_TRANSACTION;
     BEGIN
       INSERT INTO ERR_TABLE (ERR_ID, ERR_CODE, ERR_MSG) 
-      VALUES (err_table_seq.NEXTVAL, i_n_err_code, i_v_err_msg);
+      VALUES (err_table_seq.NEXTVAL, l_n_err_code, l_v_err_msg);
       COMMIT;
     END sp_err_handling;
   /*************************************************************************/
